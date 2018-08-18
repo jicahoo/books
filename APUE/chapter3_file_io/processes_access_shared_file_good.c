7f5aba9d1fb81e806027f7b0e3e3fbff27b77182
Zhang, Jichao <Jichao.Zhang@emc.com>
2018-08-18 09:21:34 -0400

    correct way to share files betweeen processes

---
+++ APUE/chapter3_file_io/processes_access_shared_file_good.c
@@ -0,0 +1,45 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFFSIZE 5
+
+
+void write_to_file(pid_t pid, char* buf) {
+    int i = 100;
+    int fd = open("./test.txt", O_APPEND|O_RDWR);
+    for(i =0 ;i < 1000; i++) {
+        ssize_t size = pwrite(fd, buf, 5, 0);
+        //printf("%d Char written: %d\n", pid, size);
+        if (size != 5) {
+            perror("Write failure\n");
+        }
+    }
+    close(fd);
+}
+
+/**
+ * Two processes are writing the same file at the same time without any sync mechanism.
+ * User expect there should be 2000 lines in test.txt. However, the actual result surprise the user.
+ */
+int main(void) {
+    printf("Hello World!\n");
+
+    pid_t pid;
+    if ( (pid = fork()) < 0 ) {
+        perror("fork failed.\n");
+    }
+
+    if (pid == 0) {
+        char buf[BUFFSIZE] = "ABCD\n";
+        write_to_file(getpid(), buf);
+    } else {
+        char buf[BUFFSIZE] = "EFGH\n";
+        write_to_file(getpid(), buf);
+        wait(NULL);
+    }
+    
+    return 0;
+}
+
+
